16e94111a89254eddfe5a75f6cf7dbdf86488f3c
rafael-m1 <rafael.muzzi2@gmail.com>
2025-07-24 14:49:09 -0400

    Altera views

--- resources/views/seasons/index.blade.php
+++ resources/views/seasons/index.blade.php
@@ -6,7 +6,7 @@
                 Temporada {{ $season->number }}
             </a>
             <span class="badge bg-secondary">
-                {{ $season-> }} / {{ $season->episodes->count() }}
+                {{ $season->episodes->filter(fn ($episode) => $episode->watched)->count() }} / {{ $season->episodes->count() }}
             </span>
         </li>
         @endforeach
--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -1,9 +1,14 @@
 <x-layout title="Séries" :mensagem-sucesso="$mensagemSucesso">
+    @auth
     <a href="{{ route("series.create") }}" class="btn btn-dark mb-2">Adicionar</a>
+    @endauth
     <ul class="list-group">
         @foreach($series as $serie)
         <li class="list-group-item d-flex justify-content-between align-items-center">
-            <a href="{{ route("seasons.index", $serie->id) }}">{{ $serie->nome }}</a>
+            @auth <a href="{{ route("seasons.index", $serie->id) }}">@endauth
+            {{ $serie->nome }}
+            @auth </a> @endauth
+            @auth
             <span class="d-flex">
                 <a href="{{ route("series.edit", $serie->id) }}" class="btn btn-primary btn-sm">Editar</a>
                 <form action="{{ route("series.destroy", $serie->id) }}" method="post" class="ms-2">
@@ -12,6 +17,7 @@
                     <button class="btn btn-danger btn-sm">Excluir</button>
                 </form>
             </span>
+            @endauth
         </li>
         @endforeach
     </ul>
